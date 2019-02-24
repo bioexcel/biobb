d4b5a3a283064c3a8be6218764834a0e0e54bb58
Pau Andrio <andriopau@gmail.com>
2019-02-24 16:56:11 +0100

    Configuring sphinx

--- biobb/docs/source/conf.py
+++ biobb/docs/source/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'm2r',
 ]
 
 # Using Google docstring style
@@ -51,10 +50,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
 
-# -- Integrate markdown ---------------------------------------------------
-source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
-}
 
 source_suffix = ['.rst', '.md']
 
