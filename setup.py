450ebbed69c92099574782181dfea18f48c9ad90
Pau Andrio <andriopau@gmail.com>
2019-05-17 21:30:15 +0200

    May 2019 Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb",
-    version="1.0.0",
+    version="1.0.1",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb module collection.",
@@ -20,7 +20,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     install_requires=['m2r',
                       'biobb_common==1.1.6',
-                      'biobb_io==1.1.6'
+                      'biobb_io==1.1.6',
                       'biobb_model==1.1.9',
                       'biobb_md==1.1.6',
                       'biobb_analysis==1.0.3',
