21ec4c503eb69adb8f22ac5a96955df59c5f8e33
gbayarri <genis.bayarri@irbbarcelona.org>
2023-10-05 12:41:42 +0200

    2023 survey

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,9 +51,9 @@ templates_path = ['_templates']
 # source_suffix = ['.rst', '.md']
 
 # -- Integrate markdown ---------------------------------------------------
-#source_parsers = {
-#   '.md': 'recommonmark.parser.CommonMarkParser',
-#}
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
 
 source_suffix = ['.rst', '.md']
 
