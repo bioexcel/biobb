eeb1be3486c7eb8198c37d4ad0352928019a185a
gbayarri <genis.bayarri@irbbarcelona.org>
2023-10-05 12:21:34 +0200

    2023 survey

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,7 +123,7 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 def setup(app):
-    app.add_stylesheet('theme_overrides.css')
+    app.add_css_file('theme_overrides.css')
     app.add_js_file('theme_overrides.js')
 
 
