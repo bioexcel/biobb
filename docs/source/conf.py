9c3203cb474fbc5a3b897a8e59493117d05a7e48
gbayarri <genis.bayarri@irbbarcelona.org>
2023-10-05 13:08:38 +0200

    2023 survey

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
-    #'recommonmark'
+    'recommonmark'
 ]
 
 # Using Google docstring style
@@ -52,9 +51,9 @@ templates_path = ['_templates']
 # source_suffix = ['.rst', '.md']
 
 # -- Integrate markdown ---------------------------------------------------
-source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
-}
+#source_parsers = {
+#   '.md': 'recommonmark.parser.CommonMarkParser',
+#}
 
 source_suffix = ['.rst', '.md']
 
